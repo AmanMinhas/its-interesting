3d461db00722e9c7231d1bd6d171f36f3a2bd1f9
AmanMinhas <amandeepSinghMinhas@gmail.com>
2014-05-27 22:14:33 -0700

    initialized with bootstrap:install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ItsInteresting::Application.config.secret_key_base = '444ac2227d39991f2d2d3e89ae0f342fd026d7627137a961a9d016e10e3902adfe4e555fa7a02347cc6ce1fdfbebe5fe64d5e17735817469e522c218531e79fc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ItsInteresting::Application.config.session_store :cookie_store, key: '_its-interesting_session'
